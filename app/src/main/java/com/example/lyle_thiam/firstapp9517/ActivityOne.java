3054781dbf443df9d57b9e9620d97e14413725f0
lylethiam54 <lylethiam@gmail.com>
2017-09-06 01:27:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lyle_thiam/firstapp9517/ActivityOne.java
@@ -0,0 +1,24 @@
+package com.example.lyle_thiam.firstapp9517;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+
+    public void nextActivity(View view)
+    {
+
+        Intent intent  = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+
+    }
+
+}
